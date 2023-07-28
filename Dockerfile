2bacc7e5025b6df3b7f49b605889344ff1e63c95
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2023-07-28 17:57:18 +0800

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM traffmonetizer/cli:latest
+RUN apk add nginx && apk add bash && mkdir traffmonetizer
+ADD storage.json /app/traffmonetizer
+ADD main.sh /app
+RUN chmod 777 /app/main.sh
+EXPOSE 80
+ENTRYPOINT ["./main.sh"]
---
+++ main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+nginx
+nginx -g daemon off;
+curl ifconfig.me ; echo
+curl ifconfig.me ; echo
+wget http://ipinfo.io/ip -O - -q ; echo
+cat /app/traffmonetizer/storage.json
+./Cli start accept --token J4VApOkLXE4EcfoZ4dTmhObOTQStOQZmaG0DKQA5E4Q=
+
