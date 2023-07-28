3c5d41ebb5e10cf4bb08bf547e757d002960d19a
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2023-07-28 18:07:49 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+nginx
+nginx -g daemon off;
 wget http://ipinfo.io/ip -O - -q ; echo
 cat /app/traffmonetizer/storage.json
 ./Cli start accept --token J4VApOkLXE4EcfoZ4dTmhObOTQStOQZmaG0DKQA5E4Q=
