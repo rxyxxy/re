2dfd9d06a8a15afbb7898f86f2e4c762ae48637b
rxyxxy <rxyxxy@gmail.com>
2023-08-27 20:46:25 +0800

    Delete main.sh

--- main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#nginx
-#nginx -g daemon off;
-curl ifconfig.me ; echo
-wget http://ipinfo.io/ip -O - -q ; echo
-cat /app/traffmonetizer/storage.json
-./Cli start accept --token J4VApOkLXE4EcfoZ4dTmhObOTQStOQZmaG0DKQA5E4Q=
-
